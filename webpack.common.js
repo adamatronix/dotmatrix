1f4be8e5e2449bf98000e40e1b2e4fbb39c98375
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2020-04-22 16:10:39 +0800

    Create special config files for dev and prod. Create common config to adhere to DRY principles

--- webpack.common.js
+++ webpack.common.js
@@ -2,9 +2,6 @@ const path = require('path')
 
 module.exports = {
   entry: './src/index.js',
-  devServer: {
-    contentBase: './lib'
-  },
   output: {
     filename: 'dot-matrix.js',
     path: path.resolve(__dirname, 'lib'),
